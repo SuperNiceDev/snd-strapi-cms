f342f7836cacb63d4f254ab69e095604baede0ac
Philipp Neumann <p.neumann@supernice-dev.com>
2024-05-09 12:38:00 +0200

    update user-permissions strapi-server

---
+++ src/extensions/users-permissions/strapi-server.ts
@@ -0,0 +1,15 @@
+module.exports = (plugin) => {
+  // plugin.contentTypes.user.lifecycles = {
+  //   async beforeFindMany(event) {
+  //     const ctx = strapi.requestContext.get();
+  //     console.log("---------------------");
+  //     console.log("---------------------");
+  //     // console.log('src/extensions/users-permissions/strapi-server.js beforeFindMany(');
+  //     console.log('src/extensions/users-permissions/strapi-server.js beforeFindMany() event', event, ctx);
+  //     console.log("---------------------");
+  //     console.log("---------------------");
+  //   },
+  // };
+
+  return plugin;
+};
