f8ebb1aaeec9eb1073bad1c984b82a340c29b100
Philipp Neumann <p.neumann@supernice-dev.com>
2024-05-10 19:56:35 +0200

    improve cron job

--- config/cron-task.ts
+++ config/cron-task.ts
@@ -1,38 +1,28 @@
 export default {
   deleteInactiveUser: {
     task: async ({ strapi }) => {
-      // console.log("cron-task strapi: ", strapi);
       const currTime = new Date().getTime();
-      // console.log("cron-task currTime: ", currTime);
-
       const users = await strapi.entityService.findMany(
         "plugin::users-permissions.user",
-        // {
-        //   populate: [
-        //     "role"
-        //   ],
-        // }
       );
-      // console.log("cron-task users: ", users);
 
       users?.forEach(async (item) => {
         const delta = currTime - parseInt(item.lastLogin);
-        if (delta > 31536000000) {
-          console.log("one year inactive!");
+        const deltaReadable = new Date(delta).toISOString().slice(11, 19);
+        console.log("cron-task deltaReadable: ", deltaReadable);
+        const yearInMilliSeconds = 1000 * 60 * 60 * 24 * 365;
+        if (delta > yearInMilliSeconds) {
           await strapi.entityService.delete(
             "plugin::users-permissions.user",
             item.id,
           );
         }
-        console.log("cron-task delta: ", delta);
-        const deltaPr = new Date(delta).toISOString().slice(11, 19);
-        console.log("cron-task deltaPr: ", deltaPr);
         return;
       });
     },
     options: {
-      // https://docs.strapi.io/dev-docs/configurations/cron#enabling-cron-jobs
-      rule: "1 * * * * *",
+      // https://docs.strapi.io/dev-docs/configurations/cron
+      rule: "0 0 1 * * 1",
     },
   },
 };
