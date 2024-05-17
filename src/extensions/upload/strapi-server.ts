eab7529e1cf674eb7df450f7a529bc70d4bbbb8b
Philipp Neumann <p.neumann@supernice-dev.com>
2024-05-17 17:01:08 +0200

    upload extension console log update

--- src/extensions/upload/strapi-server.ts
+++ src/extensions/upload/strapi-server.ts
@@ -1,7 +1,7 @@
 import imageOptimizerService from "strapi-plugin-image-optimizer/dist/server/services/image-optimizer-service";
 
 export default (plugin) => {
-  // console.log("strapi-server.ts plugin: ", plugin);
+  // console.log("src/apps/snd-strapi-cms/src/extensions/upload/strapi-server.ts: plugin: ", plugin);
   plugin.services["image-manipulation"] = imageOptimizerService;
   return plugin;
 };
