d26a70be8c4a640a5b80da7ab45e8baca8eebf69
Philipp Neumann <p.neumann@supernice-dev.com>
2025-05-02 11:55:55 +0200

    clean up strapi server users permissions extension

--- src/extensions/users-permissions/strapi-server.ts
+++ src/extensions/users-permissions/strapi-server.ts
@@ -1,44 +1,15 @@
 export default (plugin) => {
-
-  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-  // plugin.contentTypes.user.lifecycles = {
-  //   async beforeFindMany(event) {
-  //     console.log("---------------------");
-  //     console.log("src/extensions/users-permissions/strapi-server.js beforeFindMany(");
-  //     console.log("event: ", event);
-  //     const ctx = strapi.requestContext.get();
-  //     console.log("ctx: ", ctx);
-  //     console.log("---------------------");
-  //   },
-  // };
-
-  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-  // https://www.youtube.com/watch?v=2ZwiiY6tnmw
-
   plugin.controllers.user.updateMe = async (ctx) => {
-    console.log("---------------------");
-    console.log("---------------------");
-    console.log("---------------------");
-    console.log("plugin.controllers.user: ", plugin.controllers.user);
-
     if (!ctx.state.user || !ctx.state.user.id) {
       return (ctx.response.status = 401);
     }
 
     const updatedUserData = {
       ...ctx.request.body,
-      // username: ctx.state.user.username || ctx.request.body.username,
-      // email: ctx.state.user.email || ctx.request.body.email,
-      // firstname: ctx.state.user.firstname || ctx.request.body.firstname,
-      // lastname: ctx.state.user.lastname || ctx.request.body.lastname,
       lastLogin: ctx.request.body.lastLogin,
       authProvider: ctx.state.user.provider,
     };
     
-    // console.log("ctx.state.user: ", ctx.state.user);
-
     try {
       await strapi.query("plugin::users-permissions.user").update({
         where: { id: ctx.state.user.id },
@@ -51,15 +22,6 @@ export default (plugin) => {
     }
   };
 
-  // plugin.routes["content-api"].routes.push({
-  //   method: "PUT",
-  //   path: "/user/me",
-  //   handler: "user.updateMe",
-  //   config: {
-  //     prefix: "",
-  //     policies: [],
-  //   },
-  // });
   plugin.routes["content-api"].routes.unshift({
     method: "PUT",
     path: "/users/me",
