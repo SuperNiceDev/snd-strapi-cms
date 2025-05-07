a1a30c1ee27c55248c41db4a510223bf60d2d338
Philipp Neumann <p.neumann@supernice-dev.com>
2025-05-07 10:05:45 +0200

    reset server commit

--- config/server.ts
+++ config/server.ts
@@ -1,14 +1,8 @@
-import cronTasks from "./cronTasks";
-
-
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  cron: {
-    enabled: true,
-    tasks: cronTasks,
-  },
+
 });
